c5ab52138d9c1de54b8c2c707f05d31b2d2f928f
John Fritz <johnfritz@Johns-Mac.local>
2017-03-27 19:32:51 -0700

    change graphql pointer

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo BuddyBuild StartScript
-cp BUDDYBUILD_SECURE_FILES/env BUDDYBUILD_WORKSPACE/.env
+echo "BuddyBuild StartScript"
+cp $BUDDYBUILD_SECURE_FILES/env $BUDDYBUILD_WORKSPACE/.env
 
 
 
