febeeb5cb70aefb1ac8a431625832a85f14893b4
Jonathon Fritz <jfritz50@gmail.com>
2017-03-27 19:34:31 -0700

    update start script (#9)
    * use local photos for survey
    
    * change graphql pointer
    
    * change graphql pointer
    
    * change graphql pointer
    
    * change graphql pointer
    
    * change graphql pointer
    
    * change graphql pointer

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-echo BuddyBuild StartScript
-cp BUDDYBUILD_SECURE_FILES/env BUDDYBUILD_WORKSPACE/.env
+echo "BuddyBuild StartScript"
+cp $BUDDYBUILD_SECURE_FILES/env $BUDDYBUILD_WORKSPACE/.env
+
 
 
 
